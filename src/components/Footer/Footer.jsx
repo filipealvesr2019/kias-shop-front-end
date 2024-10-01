2aea01eed84cae547ed9e31adbf075f5a29a1d51
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-01 17:23:45 -0300

    refactor adiciona logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css";
-import InstagramIcon from "@mui/icons-material/Instagram";
+// import InstagramIcon from "@mui/icons-material/Instagram";
 // import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 
 export default function Footer() {
@@ -9,16 +9,16 @@ export default function Footer() {
         <div className="footerSection">
           <h3 className="footerH3">Sobre Nós</h3>
           <p className="footer__p">
-            Somos a Mediewal, uma marca de roupas masculinas criada para quem
+            {/* Somos a Mediewal, uma marca de roupas masculinas criada para quem
             quer se vestir bem e reinar no estilo. Inspirada no conceito de
-            elegância  e confiança.
+            elegância  e confiança. */}
           </p>
         </div>
         <div className="footerSection">
           <h3 className="footerH3">Contato</h3>
-          <p className="footer__p">Email: suporte@mediewal.com.br</p>
+          <p className="footer__p">Email: </p>
           {/* <p className="footer__p">Telefone: (00) 1234-5678</p> */}
-          <p className="footer__p">CNPJ: 56.171.368/0001-77</p>
+          <p className="footer__p">CNPJ: </p>
         </div>
         <div className="footerSection">
           <h3 className="footerH3">Saiba mais nas nossas redes</h3>
@@ -27,7 +27,7 @@ export default function Footer() {
               {/* <FacebookOutlinedIcon style={{ width: "35px", height: "35px" }} /> */}
             </a>
             <a href="https://www.instagram.com/lojamediewal/"  aria-label="instagram">
-              <InstagramIcon style={{ width: "35px", height: "35px" }} />
+              {/* <InstagramIcon style={{ width: "35px", height: "35px" }} /> */}
             </a>
           </div>
         </div>
