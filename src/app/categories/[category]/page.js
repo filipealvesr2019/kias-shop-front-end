9d2e15af822f98692996776b3ea3af2b7e106780
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-01 18:44:51 -0300

    refactor adiciona url

--- src/app/categories/[category]/page.js
+++ src/app/categories/[category]/page.js
@@ -86,7 +86,7 @@ export async function generateMetadata({ params }) {
     default:
       return {
         title: 'Kias Shop',
-        description: "Somos a Mediewal, uma marca de roupas masculinas criada para quem quer se vestir bem e reinar no estilo. Inspirada no conceito de elegância  e confiança.",
+        description: "",
         alternates: {
           canonical: canonicalUrl,
         },
